ac5ce4409285f3d32ab59aade0b515e6ea9ec246
ndamatta <damatta.natanael@gmail.com>
2023-12-04 18:51:14 -0300

    separated outputs and some styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 function compareLists() {
-    const listA = document.getElementById('listA').value.split('\n').map(item => item.trim());
-    const listB = document.getElementById('listB').value.split('\n').map(item => item.trim());
+    const listA = document.getElementById('followers').value.split('\n').map(item => item.trim());
+    const listB = document.getElementById('following').value.split('\n').map(item => item.trim());
   
     return listB.filter(item => !listA.includes(item)).sort();
   }
@@ -12,12 +12,16 @@ function compareLists() {
     const outputSection = document.getElementById('outputSection');
   
     if (outputList.length === 0) {
-      outputElement1.classList.add('has-text-success', 'has-background-dark', 'py-1', 'px-3')
-      outputElement1.innerText = `you don't have any unfollowerZ`;
-      outputElement2.innerText = `All the people you follow, is following you back!`;
-      outputSection.style.display = 'block';
+        outputElement1.classList.remove('has-text-danger')
+        outputElement1.classList.add('has-text-success', 'has-background-dark', 'py-1', 'px-3', 'my-2')
+        outputElement1.innerText = `you don't have any unfollowerZ`;
+        outputElement2.innerText = `All the people you follow, is following you back!`;
+        outputSection.style.display = 'block';
     } else {
-      outputElement1.innerText = outputList.join('\n');
-      outputSection.style.display = 'block';
+        outputElement1.classList.remove('has-text-success')
+        outputElement1.classList.add('has-text-danger', 'has-background-dark', 'py-1', 'px-3', 'my-2')
+        outputElement1.innerText = `you have unfollowerZ`;
+        outputElement2.innerText = outputList.join('\n');
+        outputSection.style.display = 'block';
     }
   }

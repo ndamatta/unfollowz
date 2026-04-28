77812d5572ae0ef2bed9913065e7b59edd46d259
ndamatta <damatta.natanael@gmail.com>
2026-04-28 12:41:53 -0300

    switch bg colors for better contrast

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default async function Header() {
   const t = await getTranslations('Header');
 
   return (
-    <header className="bg-rose-400">
+    <header className="bg-linear-to-l from-zinc-950 to-zinc-800">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-1 py-4">
 
         {/* logo */}
@@ -16,13 +16,13 @@ export default async function Header() {
 
         {/* nav */}
         <nav className="flex items-center gap-5">
-          <Link href="/" className="text-lg text-zinc-950 px-2 py-1 rounded-xl hover:bg-rose-300 hover:scale-105 hover:shadow-lg transition">
+          <Link href="/" className="text-base text-rose-400 px-2 py-1 rounded-xl hover:bg-rose-400 hover:text-zinc-950 hover:scale-105 hover:shadow-lg transition">
             {t("home")}
           </Link>
-          <Link href="/howitworks" className="text-lg text-zinc-950 px-2 py-1 rounded-xl hover:bg-rose-300 hover:scale-105 hover:shadow-lg transition">
+          <Link href="/howitworks" className="text-base text-rose-400 px-2 py-1 rounded-xl hover:bg-rose-400 hover:text-zinc-950 hover:scale-105 hover:shadow-lg transition">
             {t("how-it-works")}
           </Link>
-          <Link href="/about" className="text-lg text-zinc-950 px-2 py-1 rounded-xl hover:bg-rose-300 hover:scale-105 hover:shadow-lg transition">
+          <Link href="/about" className="text-base text-rose-400 px-2 py-1 rounded-xl hover:bg-rose-400 hover:text-zinc-950 hover:scale-105 hover:shadow-lg transition">
             {t("about")}
           </Link>
           <SelectLanguage />

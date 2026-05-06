cbe19a00d52b991ce2ef89badd52fb405b36df9f
ndamatta <damatta.natanael@gmail.com>
2026-05-06 14:34:16 -0300

    corrected font sizes

--- src/components/ResultSection.tsx
+++ src/components/ResultSection.tsx
@@ -9,7 +9,7 @@ function StatsBar({ followingCount, followersCount, notFollowingBack }: ProcessR
   return (
     <div className="grid grid-cols-3 gap-5 sm:gap-6 mb-8">
       <div className="flex flex-col items-center bg-zinc-800 rounded-2xl px-2 py-2 shadow-sm">
-        <span className="text-2xl sm:text-2xl md:text-3xl font-bold text-rose-200">
+        <span className="text-2xl md:text-3xl font-bold text-rose-200">
           {followersCount}
         </span>
         <span className="text-xs sm:text-sm text-slate-200 mt-1">
@@ -27,7 +27,7 @@ function StatsBar({ followingCount, followersCount, notFollowingBack }: ProcessR
       </div>
 
       <div className="flex flex-col items-center bg-zinc-800 rounded-2xl px-2 py-2 shadow-sm">
-        <span className="text-2xl sm:text-2xl md:text-3xl font-bold text-rose-200">
+        <span className="text-2xl md:text-3xl font-bold text-rose-200">
           {followingCount}
         </span>
         <span className="text-xs sm:text-sm text-slate-200 mt-1">
